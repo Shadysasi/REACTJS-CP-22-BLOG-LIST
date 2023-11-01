7ccdd78620c2b8c7549c8c1b8e8c4940a61d7845
Sasikumar <sasieng5851@gmail.com>
2023-11-01 11:21:02 +0000

    blog list app using react router dom

---
+++ src/components/BlogItem/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+
+const BlogItem = props => {
+  const {blogItem} = props
+  const {title, description, publishedDate} = blogItem
+
+  return (
+    <li className="blog-item">
+      <div className="blog-details-container">
+        <h1 className="blog-title">{title}</h1>
+        <p className="blog-date">{publishedDate}</p>
+      </div>
+      <p className="blog-description">{description}</p>
+    </li>
+  )
+}
+export default BlogItem
---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      className="profile-img"
+      alt="profile"
+    />
+    <h1 className="user-designation">Wade Warren</h1>
+    <p className="profile-job">Software Developer at UK</p>
+  </div>
+)
+export default UserInfo
